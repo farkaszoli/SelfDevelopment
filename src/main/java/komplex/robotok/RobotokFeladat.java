c98b368916decfe99f34b23e334d3820d4e0e5fd
Farkas Zoli <farkaszoli88@gmail.com>
2017-02-12 23:40:40 +0100

    legnagyobb teruletu robot hozzaadasa

--- src/main/java/komplex/robotok/RobotokFeladat.java
+++ src/main/java/komplex/robotok/RobotokFeladat.java
@@ -4,28 +4,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 //  http://www.webotlet.hu/?p=906
-public class RobotokFeladat
-{
+public class RobotokFeladat {
     private static final List<Robot> ROBOTOK_LISTAJA = Beolvas.beolvas();
 
-//    Add meg minden robot útvonalának vég koordinátáit, ha azt feltételezzük, hogy a 0;0 pontból indultak!
-    public static void koordinataSzamitas()
-    {
+    //    Add meg minden robot útvonalának vég koordinátáit, ha azt feltételezzük, hogy a 0;0 pontból indultak!
+    public static void koordinataSzamitas() {
 
     }
 
 // 2. Add meg, hogy melyik robot milyen messze került a kezdőponttól az út végére!
 
-//    3. Melyik robot áll vízszintes irányban a legmesszebb a kezdőpontjától az út végén?
-    public static void legmesszebbVizszintesen()
-    {
-        Robot legmesszebbLevoRobot = new Robot(0,0,0,0,0,0);
+    //    3. Melyik robot áll vízszintes irányban a legmesszebb a kezdőpontjától az út végén?
+    public static void legmesszebbVizszintesen() {
+        Robot legmesszebbLevoRobot = new Robot(0, 0, 0, 0, 0, 0);
         int legmesszebbLevoKoordinata = 0;
 
-        for(Robot robot : ROBOTOK_LISTAJA)
-        {
-            if(robot.getX() > legmesszebbLevoKoordinata)
-            {
+        for (Robot robot : ROBOTOK_LISTAJA) {
+            if (robot.getX() > legmesszebbLevoKoordinata) {
                 legmesszebbLevoKoordinata = robot.getX();
                 legmesszebbLevoRobot = robot;
             }
@@ -34,19 +29,17 @@ public class RobotokFeladat
         System.out.print("legmesszebb levo robot viszintesen: " + legmesszebbLevoRobot.toString());
     }
 
-//    4. Melyik robot jutott útvonala során legmesszebbre a kezdőpontjától?
+    //    4. Melyik robot jutott útvonala során legmesszebbre a kezdőpontjától?
     public static void legmesszebbjutott()
     {
-        Robot legmesszebbLevoRobot = new Robot(0,0,0,0,0,0);
+        Robot legmesszebbLevoRobot = new Robot(0, 0, 0, 0, 0, 0);
 
         int legmesszebb = 0;
 
-        for(Robot robot : ROBOTOK_LISTAJA)
-        {
+        for (Robot robot : ROBOTOK_LISTAJA) {
             int robotTavolsaga = Math.abs(robot.getX()) + Math.abs(robot.getY());
 
-            if(robotTavolsaga > legmesszebb)
-            {
+            if (robotTavolsaga > legmesszebb) {
                 legmesszebb = robotTavolsaga;
                 legmesszebbLevoRobot = robot;
             }
@@ -56,16 +49,36 @@ public class RobotokFeladat
 
     }
 
-//  5. Add meg, melyek azok a robotok, melyek túlmentek volna egy 21×21-es tábla határain, melynek
+    //  5. Add meg, melyek azok a robotok, melyek túlmentek volna egy 21×21-es tábla határain, melynek
 //  a középpontjából indultak!
-    public static void tulmentek()
-    {
+    public static void tulmentek() {
         List<Robot> hataronBeluliRobotok = ROBOTOK_LISTAJA.stream()
                 .filter(robot -> robot.getX() > 21)
                 .filter(robot -> robot.getY() > 21)
                 .collect(Collectors.toList());
 
         System.out.print("tulmentek: " + hataronBeluliRobotok);
+    }
+
+    //    9. Melyik roboté a legnagyobb terület?
+    public static void legnagyobbTerulet()
+    {
+        Robot legnagyobbTeruletuRobot = new Robot(0, 0, 0, 0, 0, 0);
+        int legnagyobbTerulet = 0;
+        int terulet;
+
+        for (Robot robot : ROBOTOK_LISTAJA)
+        {
+            terulet = Math.abs(robot.getX()) * Math.abs(robot.getX());
+
+            if ( terulet > legnagyobbTerulet)
+            {
+                legnagyobbTerulet = terulet;
+                legnagyobbTeruletuRobot = robot;
+            }
+        }
+
+        System.out.print("legnagyobbTeruletu robot: " + legnagyobbTeruletuRobot);
     }
 
     // 10. Van-e olyan robot, melynek útvonala tartalmaz szabályos négyzetet?
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,6 +11,6 @@ public class Main
     {
         System.out.println("Self Development Home project");
 
-        RobotokFeladat.szabalyosNegyzetAMegtettUt();
+        RobotokFeladat.legnagyobbTerulet();
     }
 }
