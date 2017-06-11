bf8923627091c58fd65a0647db22f2b486aba563
Farkas Zoli <farkaszoli88@gmail.com>
2017-06-11 23:24:53 +0200

    kuldetesek beallitasa

---
+++ src/main/java/jatekok/shakesandfidgetlite/kuldetes/Kuldetes.java
@@ -0,0 +1,16 @@
+package jatekok.shakesandfidgetlite.kuldetes;
+
+public class Kuldetes
+{
+    private int kultedesSzint;
+
+    public Kuldetes(int kultedesSzint)
+    {
+        this.kultedesSzint = kultedesSzint;
+    }
+
+    public int getKultedesSzint()
+    {
+        return kultedesSzint;
+    }
+}
