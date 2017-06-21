c6814caa3c2ec3da8e9eb69b1ace0b3b200489d5
Farkas Zoli <farkaszoli88@gmail.com>
2017-06-21 23:50:35 +0200

    szamitasok elvegzese a kuldetesekhez kulon metodusokban

--- src/main/java/jatekok/shakesandfidgetlite/kuldetes/Kuldetes.java
+++ src/main/java/jatekok/shakesandfidgetlite/kuldetes/Kuldetes.java
@@ -2,11 +2,14 @@ package jatekok.shakesandfidgetlite.kuldetes;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class Kuldetes
 {
     private int kultedesSzint;
     private int tapasztalatiPont = 0;
+    private int kuldetesSzam = 0;
+    private int aranySzama;
 
     public Kuldetes(int kultedesSzint)
     {
@@ -26,4 +29,20 @@ public class Kuldetes
         }
     }
 
+    public int getKuldetesSzam()
+    {
+        return kuldetesSzam;
+    }
+
+    private int kuldetesertJaroArany(int kuldetesSzint)
+    {
+        Random r = new Random();
+
+        if( kuldetesSzint == 1)
+        {
+         return r.nextInt(10) + 30;
+        }
+
+        return r.nextInt(15) + 30;
+    }
 }
