6e791317a2c0bfa33e6053aa6c053ce19dab3611
Farkas Zoli <farkaszoli88@gmail.com>
2017-06-22 00:20:43 +0200

    kuldetes reszben szereplo szamitasok elvegzese

--- src/main/java/jatekok/shakesandfidgetlite/kuldetes/Kuldetes.java
+++ src/main/java/jatekok/shakesandfidgetlite/kuldetes/Kuldetes.java
@@ -10,6 +10,7 @@ public class Kuldetes
     private int tapasztalatiPont = 0;
     private int kuldetesSzam = 0;
     private int aranySzama;
+    private double qp = 80;
 
     public Kuldetes(int kultedesSzint)
     {
@@ -25,7 +26,7 @@ public class Kuldetes
     {
         if( tapasztalatiPont > 100)
         {
-
+            // TODO next commit
         }
     }
 
@@ -45,4 +46,36 @@ public class Kuldetes
 
         return r.nextInt(15) + 30;
     }
+
+    public void harc(int eletero, int maxEletero, int szint, int kuldetesSzint)
+    {
+        if ( eletero / maxEletero >= 0.3)
+        {
+            if (kuldetesSzint == 1)
+            {
+                csata();
+                qp = qp * 1.12;
+            } else
+            {
+                csata();
+                qp = qp * 1.20;
+            }
+        }
+        else
+        {
+            System.out.println("keves az eleteronk!");
+        }
+    }
+
+    public double qpSzamitasa(int szint)
+    {
+        return szint * 0.66;
+    }
+
+    private void csata()
+    {
+        aranySzama++; tapasztalatiPont++;
+    }
+
+
 }
