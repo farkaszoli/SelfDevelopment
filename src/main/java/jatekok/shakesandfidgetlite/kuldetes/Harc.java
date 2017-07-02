028fd78cbd0ad24e471811934e540cddccc225f3
Farkas Zoli <farkaszoli88@gmail.com>
2017-07-02 18:58:55 +0200

    harc osztaly bovitese

--- src/main/java/jatekok/shakesandfidgetlite/kuldetes/Harc.java
+++ src/main/java/jatekok/shakesandfidgetlite/kuldetes/Harc.java
@@ -13,7 +13,26 @@ public class Harc
 
     private void harc(Kaszt jatekos, Kaszt gepiEllenfel)
     {
+        int jatekosEllenallasa = jatekos.getErtelem() / 2;
+        int gepEllenallasa = gepiEllenfel.getErtelem() / 2;
 
+        // 1. kör
+        gepiEllenfel.setSebzes(gepiEllenfel.getSebzes() - jatekosEllenallasa);
+        jatekos.setSebzes(jatekos.getSebzes() - gepEllenallasa);
+
+        // 2. körtől
+        gepiEllenfel.setSebzes(gepiEllenfel.getSebzes() - (jatekosEllenallasa / 2));
+        jatekos.setSebzes(jatekos.getSebzes() - (gepEllenallasa / 2));
+        /*
+        Van lehetőség a kitérésre is, kitérni mindig a védekező fél tud,
+        ami úgy alakul, hogy a védekező ügyességéből kivonjuk a támadó értelem értékét, amennyiben ez
+        pozitív szám, akkor a védekező 25% eséllyel nem szenved el sebzést. Amennyiben ez az érték 0
+        vagy kisebb lenne, akkor a támadó biztosan eltalálja a védekezőt.
+         */
+
+        // kitérés
+        jatekos.setUgyesseg(jatekos.getUgyesseg() - gepEllenallasa);
+        gepiEllenfel.setUgyesseg(gepiEllenfel.getUgyesseg() - jatekosEllenallasa);
     }
 
     private Kaszt ellenfelLetrehozasa(Kaszt jatekos)
